161bba101405ca71db98bb8e2e60ce2f30c3e950
ginger <26461046+gingerchew@users.noreply.github.com>
2024-12-30 17:27:47 -0500

    Adds unocss to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import Unocss from 'unocss/astro';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://bycosmo.github.io',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), Unocss({
+		injectReset: true
+	})],
 });
