4cce19bb88f1bf1f904ef12207c077cb7aa03f2a
ginger <26461046+gingerchew@users.noreply.github.com>
2025-01-03 11:54:30 -0500

    Adjusts the spacing in the site title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'bycosmo ☄️';
+export const SITE_TITLE = 'bycosmo☄️';
 export const SITE_DESCRIPTION = 'An opinionated framework for building websites with Web Components';
