238d54946962e6d92bff5216efe653533b7997fa
ginger <26461046+gingerchew@users.noreply.github.com>
2025-01-20 11:48:07 -0500

    Hopefully this fixes the `blog/blog/` issue with the blogroll

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import Unocss from 'unocss/astro';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://bycosmo.github.io',
+	base: '',
 	integrations: [mdx(), sitemap(), Unocss({
 		injectReset: true
 	})],
